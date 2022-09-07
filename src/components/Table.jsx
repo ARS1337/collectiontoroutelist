56eea4603e0f703f83091c45ee959d83b2246def
abhaysingh <abhaysingh17498@gmail.com>
2022-09-07 18:57:37 +0530

    .

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,36 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
 
 function Table(props) {
-  console.log(JSON.parse(props.file));
+  const [file, setFile] = useState([]);
+  const [allData, setAllData] = useState([]);
+  const [variableData, setVariableData] = useState([]);
+
+  useEffect(() => {
+    if (props.file) {
+      let newFile1 = JSON.parse(props.file);
+      let newFile = JSON.parse(newFile1);
+      console.log(newFile);
+      let rows = newFile.item;
+      setFile(rows);
+      setAllData(newFile);
+      setVariableData(newFile.variable);
+    }
+  }, [props.file]);
+
   const columns = [
     {
-      name: "Title",
-      selector: (row) => row.title,
+      name: "name",
+      selector: (row) => row.name,
     },
     {
-      name: "Year",
-      selector: (row) => row.year,
+      name: "method",
+      selector: (row) => row.request.method,
+    },
+    {
+      name: "path",
+      selector: (row) => row.request.url.raw,
+    },
+    {
+      name: "action",
+      selector: (row) => row.name,
     },
   ];
 
-  const data = [
+  const variablesColumn = [
     {
-      id: 1,
-      title: "Beetlejuice",
-      year: "1988",
+      name: "name",
+      selector: (row) => row.key,
     },
     {
-      id: 2,
-      title: "Ghostbusters",
-      year: "1984",
+      name: "value",
+      selector: (row) => row.value,
     },
   ];
 
   return (
     <div className="p-4 rounded-md font-mono">
       {props.file ? (
-        <DataTable columns={columns} data={data} />
+        <div className="">
+          <DataTable columns={columns} data={file ? file : {}} />
+          <div className="p-1" />
+          <DataTable columns={variablesColumn} data={file ? variableData : {}} />
+        </div>
       ) : (
         <p className="font-mono">{"Please select a collection"}</p>
       )}
